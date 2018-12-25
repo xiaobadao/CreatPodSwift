05ce9b4989065b8a0e81826f87fe6ae4741b3253
chenglong <chenglong@princeegg.com>
2018-12-25 16:50:06 +0800

    创建pod库模版
    创建pod库模版

---
+++ CreatPodSwift/classes/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  CreatPodSwift
+//
+//  Created by apple on 2018/12/25.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
